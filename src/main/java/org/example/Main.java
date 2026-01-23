e7a81e999db9c0233c013b204b5f5f793208102b
KsanaVasileva <oksanchikf1991@gmail.com>
2026-01-23 22:07:50 +0300

    Add HW2

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,46 @@
+package org.example;
+
+//1. Необходимо вывести на экран числа от 1 до 10. На экране должно быть:
+//1 2 3 4 5 6 7 8 9 10
+//public class Main {
+//    public static void main(String[] args) {
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.print(i + " ");
+//        }
+//    }
+//}
+
+//2. Необходимо вывести на экран числа от 10 до 5. На экране должно быть:
+//10 9 8 7 6 5
+//        public class Main {
+//            public static void main(String[] args) {
+//                int i = 10;
+//
+//                do {
+//                    System.out.print(i + " ");
+//                    i--;
+//                } while (i >= 5);
+//            }
+//        }
+
+//3.    Необходимо вывести на экран таблицу умножения на 3:
+//            3*1=3
+//            3*2=6
+//            3*3=9
+//            3*4=12
+//            3*5=15
+//            3*6=18
+//            3*7=21
+//            3*8=24
+//            3*9=27
+//            3*10=30
+
+//public class Main {
+//    public static void main (String[] args) {
+//        for (int i = 0; i <= 10 ; i++) {
+//            int res = 3 * i;
+//            System.out.println("3*" + i + "=" + res);
+//        }
+//    }
+//}
+
