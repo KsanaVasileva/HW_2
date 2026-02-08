b63f8a712fc62c043b67049189794d30f7b6cdd2
KsanaVasileva <oksanchikf1991@gmail.com>
2026-02-08 12:19:53 +0300

    Add fix structure

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,46 +1,41 @@
 package org.example;
 
-1. Необходимо вывести на экран числа от 1 до 10. На экране должно быть:
-1 2 3 4 5 6 7 8 9 10
 public class Main {
-    public static void main(String[] args) {
+
+    // 1. Необходимо вывести на экран числа от 1 до 10. На экране должно быть:
+    //1 2 3 4 5 6 7 8 9 10
+    public static void printNumbersFrom1To10() {
+        System.out.println("Необходимо вывести на экран числа от 1 до 10");
         for (int i = 1; i <= 10; i++) {
             System.out.print(i + " ");
         }
+        System.out.println();
     }
-}
 
-2. Необходимо вывести на экран числа от 10 до 5. На экране должно быть:
-10 9 8 7 6 5
-        public class Main {
-            public static void main(String[] args) {
-                int i = 10;
+    // 2. Необходимо вывести на экран числа от 10 до 5. На экране должно быть:
+    //10 9 8 7 6 5
+    public static void printNumbersFrom10To5() {
+        System.out.println(" Необходимо вывести на экран числа от 10 до 5");
+        int i = 10;
+        do {
+            System.out.print(i + " ");
+            i--;
+        } while (i >= 5);
+        System.out.println();
+    }
 
-                do {
-                    System.out.print(i + " ");
-                    i--;
-                } while (i >= 5);
-            }
-        }
-
-3.    Необходимо вывести на экран таблицу умножения на 3:
-            3*1=3
-            3*2=6
-            3*3=9
-            3*4=12
-            3*5=15
-            3*6=18
-            3*7=21
-            3*8=24
-            3*9=27
-            3*10=30
-
-public class Main {
-    public static void main (String[] args) {
-        for (int i = 0; i <= 10 ; i++) {
+    // 3.    Необходимо вывести на экран таблицу умножения на 3:
+    public static void printMultiplicationTableBy3() {
+        System.out.println("Таблица умножения на 3");
+        for (int i = 1; i <= 10; i++) {
             int res = 3 * i;
             System.out.println("3*" + i + "=" + res);
         }
     }
+    public static void main(String[] args) {
+        printNumbersFrom1To10();
+        printNumbersFrom10To5();
+        printMultiplicationTableBy3();
+     }
 }
 
